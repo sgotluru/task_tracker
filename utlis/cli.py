ecf392cb6ba9ffea162b2d2f4e0b837ebfd1a462
sreekara_gotluru <sgotluru@aum.edu>
2025-01-18 23:31:13 -0600

    add new error handling in cli.py for main

--- utlis/cli.py
+++ utlis/cli.py
@@ -9,16 +9,23 @@ FILE_PATH = "tasks.json" # Path(__file__).parent / "tasks.json"
 def main():
     if not os.path.exists(FILE_PATH):
         initialize_json_file(FILE_PATH)
+        
+    try:
+        args = get_parsed_arguments()
 
-    args = get_parsed_arguments()
-
-    if args.command == "add":
-        add(args)
-    elif args.command == "update":
-        update(args)
-    elif args.command == "delete":
-        delete(args)
-    elif args.command == "list":
-        list_status(args)
-    else:
-        print("Invalid command. Use --help for usage information.")
+        if args.command == "add":
+            add(args)
+        elif args.command == "update":
+            update(args)
+        elif args.command == "delete":
+            delete(args)
+        elif args.command == "mark-in-progress":
+            update(args, "in-progress")
+        elif args.command == "mark-done":
+            update(args, "done")
+        elif args.command == "list":
+            list_status(args)
+        else:
+            print("Invalid command. Use --help for usage information.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
